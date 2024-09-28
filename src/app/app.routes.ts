e6c87f1f3585e5feebadae6bc828e54638431b63
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-09-27 23:53:13 -0300

    Configurando rota para a classe Autor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AutorFormComponent } from './components/autor/autor-form/autor-form.component';
+import { AutorListComponent } from './components/autor/autor-list/autor-list.component';
+import { autorResolver } from './components/autor/resolver/autor.resolver';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'autores',component: AutorListComponent, title: 'Lista de Autores'},
+    {path: 'autores/new',component: AutorFormComponent, title: 'Novo Autor'},
+    {path: 'autores/edit/:id', component: AutorFormComponent, resolve: {autor: autorResolver}}
+];
