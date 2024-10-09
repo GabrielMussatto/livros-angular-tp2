7ff5255ee3c981d5669152fccde62e3213d0badc
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-10-09 10:21:09 -0300

    Criando caixaLivro-list

---
+++ src/app/components/caixaLivro/caixa-livro-list/caixa-livro-list.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { NgFor } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { RouterModule } from '@angular/router';
+import { CaixaLivroService } from '../../../services/caixa-livro.service';
+import { CaixaLivro } from '../../../models/caixa-livro.model';
+import { MatDialog } from '@angular/material/dialog'; // Importar MatDialog
+import { ConfirmationDialogComponent } from '../../dialog/confirmation-dialog/confirmation-dialog.component'; // Importar o componente de dialogo
+
+@Component({
+  selector: 'app-caixa-livro-list',
+  standalone: true,
+  imports: [NgFor, MatToolbarModule, MatIconModule, MatButtonModule, MatTableModule, RouterModule],
+  templateUrl: './caixa-livro-list.component.html',
+  styleUrl: './caixa-livro-list.component.css'
+})
+export class CaixaLivroListComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'nome', 'descricao', 'quantidadeEstoque', 'fornecedor', 'editora', 'genero', 'autor', 'classificacao', 'acao'];
+  caixaLivros: CaixaLivro[] = [];
+
+  constructor(
+    private caixaLivroService: CaixaLivroService,
+    private dialog: MatDialog
+  ) { }
+
+  ngOnInit(): void {
+    this.caixaLivroService.findAll().subscribe(
+      data => { this.caixaLivros = data; }
+    );
+  }
+
+  excluir(caixaLivro: CaixaLivro): void {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '300px',
+      data: {
+        message: 'Deseja realmente excluir esta Caixa de Livro? Não será possível reverter.'
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        this.caixaLivroService.delete(caixaLivro).subscribe({
+          next: () => {
+            this.caixaLivros = this.caixaLivros.filter(e => e.id !== caixaLivro.id);
+          },
+          error: (err) => {
+            console.error('Erro ao tentar excluir a Caixa de Livros', err);
+          }
+        });
+      }
+    });
+  }
+}
