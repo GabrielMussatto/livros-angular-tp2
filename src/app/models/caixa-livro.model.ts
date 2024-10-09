c9d22a956b439ac80f6786d1826f0d408816c948
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-10-09 10:20:00 -0300

    Criando models de Livro e CaixaLivro

---
+++ src/app/models/caixa-livro.model.ts
@@ -0,0 +1,17 @@
+import { Autor } from "./autor.model";
+import { Editora } from "./editora.model";
+import { Fornecedor } from "./fornecedor.model";
+import { Genero } from "./genero.model";
+
+export class CaixaLivro {
+    id!: number;
+    nome!: string;
+    descricao!: string;
+    quantidadeEstoque!: number;
+    fornecedor!: Fornecedor;
+    editora!: Editora;
+    generos!: Genero[];
+    autores!: Autor[];
+    classificacao!: number;
+    //fazer preco depois
+}
