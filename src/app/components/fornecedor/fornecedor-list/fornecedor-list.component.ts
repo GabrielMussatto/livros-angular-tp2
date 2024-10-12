078d3748da35c991908600b10fee25618fa1d27f
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-10-12 01:04:37 -0300

    Adicionando menu em fornecedor-list

--- src/app/components/fornecedor/fornecedor-list/fornecedor-list.component.ts
+++ src/app/components/fornecedor/fornecedor-list/fornecedor-list.component.ts
@@ -1,39 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { Fornecedor } from '../../../models/fornecedor.model';  // Atualizado para usar o modelo de Fornecedor
-import { FornecedorService } from '../../../services/fornecedor.service';  // Atualizado para usar o serviço de Fornecedor
+import { Fornecedor } from '../../../models/fornecedor.model';
+import { FornecedorService } from '../../../services/fornecedor.service';
 import { NgFor } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
-import { RouterModule } from '@angular/router';
-import { MatDialog } from '@angular/material/dialog'; // Importa MatDialog
-import { ConfirmationDialogComponent } from '../../dialog/confirmation-dialog/confirmation-dialog.component'; // Importa o componente de diálogo de confirmação
+import { Router, RouterModule } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationDialogComponent } from '../../dialog/confirmation-dialog/confirmation-dialog.component';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-fornecedor-list',
   standalone: true,
-  imports: [NgFor, MatToolbarModule, MatIconModule, MatButtonModule, MatTableModule, RouterModule],
+  imports: [NgFor, MatToolbarModule, MatIconModule, MatButtonModule, MatTableModule, RouterModule, MatMenuModule],
   templateUrl: './fornecedor-list.component.html',
   styleUrls: ['./fornecedor-list.component.css']
 })
 export class FornecedorListComponent implements OnInit {
-  fornecedores: Fornecedor[] = [];  // Atualizado para usar Fornecedor
-  displayedColumns: string[] = ['id', 'nome', 'cnpj', 'email', 'endereco', 'telefone', 'acao'];  // Atualizado para incluir CNPJ
+  fornecedores: Fornecedor[] = [];
+  displayedColumns: string[] = ['id', 'nome', 'cnpj', 'email', 'endereco', 'telefone', 'acao'];
 
   constructor(
     private fornecedorService: FornecedorService,
-    private dialog: MatDialog // Injeta o MatDialog
+    private dialog: MatDialog,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.fornecedorService.findAll().subscribe(
-      data => { this.fornecedores = data; }  // Atualizado para atribuir os dados ao array de fornecedores
+      data => { this.fornecedores = data; }
     );
   }
 
-  excluir(fornecedor: Fornecedor): void {  // Atualizado para usar Fornecedor
-    // Abre o diálogo de confirmação
+  excluir(fornecedor: Fornecedor): void {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
       data: { message: 'Deseja realmente excluir este Fornecedor? Não será possível reverter.' }
     });
@@ -42,13 +43,35 @@ export class FornecedorListComponent implements OnInit {
       if (result) {
         this.fornecedorService.delete(fornecedor).subscribe({
           next: () => {
-            this.fornecedores = this.fornecedores.filter(f => f.id !== fornecedor.id);  // Atualizado para remover o fornecedor da lista
+            this.fornecedores = this.fornecedores.filter(f => f.id !== fornecedor.id);
           },
           error: (err) => {
-            console.error("Erro ao tentar excluir o fornecedor", err);  // Atualizado para mensagem de erro de fornecedor
+            console.error("Erro ao tentar excluir o fornecedor", err);
           }
         });
       }
     });
+  }
+
+  editora() {
+    this.router.navigateByUrl('/editoras');
+  }
+  autor() {
+    this.router.navigateByUrl('/autores');
+  }
+  caixaLivros() {
+    this.router.navigateByUrl('/caixaLivros');
+  }
+  livro() {
+    this.router.navigateByUrl('/livros');
+  }
+  genero() {
+    this.router.navigateByUrl('/generos');
+  }
+  fornecedor() {
+    this.router.navigateByUrl('/fornecedores');
+  }
+  voltar() {
+    this.router.navigateByUrl('/fornecedores');
   }
 }
