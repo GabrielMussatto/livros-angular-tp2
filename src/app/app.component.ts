06ca3f0798dba10ce7157d9979529bbe286413b4
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-11-08 22:56:41 -0300

    Adicionando Card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'livros-angular-tp2';
+
+  constructor(
+    private router: Router
+  ) { }
+
+  editora() {
+    this.router.navigateByUrl('/admin/editoras');
+  }
+  autor() {
+    this.router.navigateByUrl('/admin/autores');
+  }
+  caixaLivros() {
+    this.router.navigateByUrl('/admin/caixaLivros');
+  }
+  livro() {
+    this.router.navigateByUrl('/admin/livros');
+  }
+  genero() {
+    this.router.navigateByUrl('/admin/generos');
+  }
+  fornecedor() {
+    this.router.navigateByUrl('/admin/fornecedores');
+  }
 }
