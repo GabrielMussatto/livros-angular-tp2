7c996334b08c68bef36e37b55b0d019934ca70ed
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-11-13 23:37:47 -0300

    Adicionando carregamento de pagina em livro detalhado

--- src/app/components/livro/livro-detalhado-list/livro-detalhado-list.component.ts
+++ src/app/components/livro/livro-detalhado-list/livro-detalhado-list.component.ts
@@ -4,16 +4,18 @@ import { LivroService } from '../../../services/livro.service';
 import { Livro } from '../../../models/livro.model';
 import { NgFor, NgIf } from '@angular/common';
 import { MatCard, MatCardActions, MatCardContent, MatCardFooter, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-livro-detalhado-list',
   standalone: true,
-  imports: [MatCard, MatCardActions, MatCardContent, MatCardFooter, MatCardHeader, MatCardTitle, MatCardSubtitle, NgFor, NgIf],
+  imports: [MatCard, MatCardActions, MatCardContent, MatCardFooter, MatCardHeader, MatCardTitle, MatCardSubtitle, NgFor, NgIf, MatProgressSpinner],
   templateUrl: './livro-detalhado-list.component.html',
   styleUrls: ['./livro-detalhado-list.component.css']
 })
 export class LivroDetalhadoListComponent implements OnInit {
   livro: Livro | undefined;
+  carregando = true;
 
   constructor(
     private route: ActivatedRoute,
@@ -25,11 +27,14 @@ export class LivroDetalhadoListComponent implements OnInit {
     if (idLivro) {
       this.livroService.findById(idLivro).subscribe(
         (livro) => {
-          console.log(livro);
-          this.livro = livro;
+          setTimeout(() => {
+            this.livro = livro;
+            this.carregando = false;
+          }, 200);
         },
         (error) => {
           console.error('Erro ao carregar o livro', error);
+          this.carregando = false;
         }
       );
     }
