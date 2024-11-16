c61a56c9adf538f275b353abd9051a479ce206c8
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-11-16 03:09:51 -0300

    Criando Resolver para livroDetalhado

---
+++ src/app/components/livro/resolver/livro-detalhado.resolver.ts
@@ -0,0 +1,10 @@
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
+import { inject } from '@angular/core';
+import { map } from 'rxjs';
+import { Livro } from '../../../models/livro.model';
+import { LivroService } from '../../../services/livro.service';
+
+export const livroDetalhadoResolver: ResolveFn<Livro> = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  return inject(LivroService).findByNome(route.paramMap.get('titulo')!)
+    .pipe(map(livro => livro[0]));
+};
