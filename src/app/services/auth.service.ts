6816fede6d4208ceacc1ff16cf50d6c9e44bf5a7
MenesesErick <124937575+MenesesErick@users.noreply.github.com>
2024-11-18 23:48:46 -0300

    Implementando auth.service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,93 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { Usuario } from '../models/usuario.model';
+import { LocalStorageService } from './local-storage.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private baseUrl = 'http://localhost:8080/auth';
+  private tokenKey = 'jwt_token';
+  private usuarioLogadoKey = 'usuario_logado';
+  private usuarioLogadoSubject = new BehaviorSubject<Usuario | null>(null);
+
+  constructor(
+    private httpClient: HttpClient,
+    private localStorageService: LocalStorageService,
+    private jwtHelper: JwtHelperService
+  ) {
+    this.initUsuarioLogado();
+  }
+
+  private initUsuarioLogado(): void {
+    const usuario = this.localStorageService.getItem(this.usuarioLogadoKey);
+    if (usuario) {
+      this.usuarioLogadoSubject.next(JSON.parse(usuario));
+    }
+  }
+
+  public login(username: string, senha: string, perfil: number): Observable<any> {
+    const params = {
+      username,
+      senha,
+      perfil
+    };
+
+    return this.httpClient.post(`${this.baseUrl}`, params, { observe: 'response' }).pipe(
+      tap((res: any) => {
+        const authToken = res.headers.get('Authorization') ?? '';
+        if (authToken) {
+          this.setToken(authToken);
+          const usuarioLogado = res.body;
+          console.log(usuarioLogado);
+          if (usuarioLogado) {
+            this.setUsuarioLogado(usuarioLogado);
+            this.usuarioLogadoSubject.next(usuarioLogado);
+          }
+        }
+      })
+    );
+  }
+
+  setUsuarioLogado(usuario: Usuario): void {
+    this.localStorageService.setItem(this.usuarioLogadoKey, JSON.stringify(usuario));
+  }
+
+  setToken(token: string): void {
+    this.localStorageService.setItem(this.tokenKey, token);
+  }
+
+  getUsuarioLogado(): Observable<Usuario | null> {
+    return this.usuarioLogadoSubject.asObservable();
+  }
+
+  getToken(): string | null {
+    return this.localStorageService.getItem(this.tokenKey);
+  }
+
+  removeToken(): void {
+    this.localStorageService.removeItem(this.tokenKey);
+  }
+
+  removeUsuarioLogado(): void {
+    this.localStorageService.removeItem(this.usuarioLogadoKey);
+    this.usuarioLogadoSubject.next(null);
+  }
+
+  isTokenExpired(): boolean {
+    const token = this.getToken();
+    if (!token) {
+      return true;
+    }
+
+    try {
+      return this.jwtHelper.isTokenExpired(token);
+    } catch (error) {
+      console.error('Token inválido', error);
+      return true;
+    }
+  }
+}
