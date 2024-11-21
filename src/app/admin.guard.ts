a1f709d80ed3691ff9e0d97d57f3536595e0fa0a
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-11-21 00:21:17 -0300

    Ajuste no guard

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -1,32 +1,32 @@
-import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
-import { Router } from '@angular/router';
-import { map } from 'rxjs/operators';
-import { AuthService } from './services/auth.service';
-import { Observable } from 'rxjs';
+// import { Injectable } from '@angular/core';
+// import { CanActivate } from '@angular/router';
+// import { Router } from '@angular/router';
+// import { map } from 'rxjs/operators';
+// import { AuthService } from './services/auth.service';
+// import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AdminGuard implements CanActivate {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AdminGuard implements CanActivate {
 
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
+//   constructor(
+//     private authService: AuthService,
+//     private router: Router
+//   ) {}
 
-  canActivate(): Observable<boolean> {
-    return this.authService.getUsuarioLogado().pipe(
-      map((usuario) => {
-        const tokenValido = !this.authService.isTokenExpired();
+//   canActivate(): Observable<boolean> {
+//     return this.authService.getUsuarioLogado().pipe(
+//       map((usuario) => {
+//         const tokenValido = !this.authService.isTokenExpired();
 
-        if (!usuario || !tokenValido || usuario.perfil !== 1) {
-          this.router.navigate(['/inicio']);  // Redireciona para a página inicial se não for admin
-          return false;
-        }
+//         if (!usuario || !tokenValido || usuario.perfil !== 1) {
+//           this.router.navigateByUrl('/inicio');  // Redireciona para a página inicial se não for admin
+//           return false;
+//         }
 
-        return true;
-      })
-    );
-  }
-}
+//         return true;
+//       })
+//     );
+//   }
+// }
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,39 +1,39 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { map, Observable } from 'rxjs';
-import { AuthService } from './services/auth.service';
+// import { Injectable } from '@angular/core';
+// import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+// import { map, Observable } from 'rxjs';
+// import { AuthService } from './services/auth.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuard implements CanActivate {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthGuard implements CanActivate {
 
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-  ) {}
+//   constructor(
+//     private authService: AuthService,
+//     private router: Router,
+//   ) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean> | Promise<boolean> | boolean {
-    return this.authService.getUsuarioLogado().pipe(
-      map((usuario) => {
-        const tokenValido = !this.authService.isTokenExpired();
+//   canActivate(
+//     route: ActivatedRouteSnapshot,
+//     state: RouterStateSnapshot
+//   ): Observable<boolean> | Promise<boolean> | boolean {
+//     return this.authService.getUsuarioLogado().pipe(
+//       map((usuario) => {
+//         const tokenValido = !this.authService.isTokenExpired();
 
-        if (!usuario || !tokenValido) {
-          this.router.navigateByUrl('/login');  // Redireciona para o login se não estiver logado ou o token expirou
-          return false;
-        }
+//         if (!usuario || !tokenValido) {
+//           this.router.navigateByUrl('/login');  // Redireciona para o login se não estiver logado ou o token expirou
+//           return false;
+//         }
 
-        const perfilRequerido = route.data['perfil']; // Perfil que a rota exige
-        if (perfilRequerido && usuario.perfil !== perfilRequerido) {
-          this.router.navigateByUrl('/login');  // Redireciona se o perfil não for permitido
-          return false;
-        }
+//         const perfilRequerido = route.data['perfil']; // Perfil que a rota exige
+//         if (perfilRequerido && usuario.perfil !== perfilRequerido) {
+//           this.router.navigateByUrl('/login');  // Redireciona se o perfil não for permitido
+//           return false;
+//         }
 
-        return true;  // Acesso permitido
-      })
-    );
-  }
-}
+//         return true;  // Acesso permitido
+//       })
+//     );
+//   }
+// }
