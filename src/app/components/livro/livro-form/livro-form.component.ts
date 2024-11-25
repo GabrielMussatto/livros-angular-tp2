78d89ea51fa93cd31cb950d23f12916f89887bb1
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-11-25 14:24:51 -0300

    Adicionando local para fazer upload da imagem em livro form

--- src/app/components/livro/livro-form/livro-form.component.ts
+++ src/app/components/livro/livro-form/livro-form.component.ts
@@ -1,11 +1,11 @@
-import { NgFor, NgIf } from '@angular/common';
+import { CommonModule, Location, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
+import { MatSelect, MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
@@ -27,13 +27,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { Classificacao } from '../../../models/classificacao.model';
 
 @Component({
   selector: 'app-livro-form',
   standalone: true,
   imports: [NgFor, ReactiveFormsModule, MatCardModule, MatFormFieldModule,
-    MatButtonModule, NgIf, MatInputModule, RouterModule, MatTableModule, MatToolbarModule, MatSelectModule, MatDatepickerModule,
-    MatNativeDateModule, MatIconModule, MatMenuModule, MatSnackBarModule],
+    MatButtonModule, NgIf, MatInputModule, RouterModule, MatTableModule, MatToolbarModule, MatSelectModule, MatSelect, MatDatepickerModule,
+    MatNativeDateModule, MatIconModule, MatMenuModule, MatSnackBarModule, CommonModule],
   templateUrl: './livro-form.component.html',
   styleUrls: ['./livro-form.component.css']
 })
@@ -43,7 +44,11 @@ export class LivroFormComponent implements OnInit {
   editoras: Editora[] = [];
   generos: Genero[] = [];
   autores: Autor[] = [];
+  classificacoes: Classificacao[] = [];
 
+  fileName: string = '';
+  selectedFile: File | null = null;
+  imagePreview: string | ArrayBuffer | null = null;
 
   constructor(private formBuilder: FormBuilder,
     private livroService: LivroService,
@@ -54,54 +59,63 @@ export class LivroFormComponent implements OnInit {
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private dialog: MatDialog,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location
   ) {
 
     this.formGroup = this.formBuilder.group({
       id: [null],
       titulo: ['', Validators.required],
       descricao: ['', Validators.required],
-      quantidadeEstoque: [null, Validators.required],
+      quantidadeEstoque: ['', Validators.required],
       isbn: ['', Validators.required],
-      preco: [null, Validators.required],
+      preco: ['', Validators.required],
       fornecedor: [null, Validators.required],
       editora: [null, Validators.required],
       generos: [[], Validators.required],
       autores: [[], Validators.required],
-      classificacao: ['', Validators.required],
+      classificacao: [null, Validators.required],
       datalancamento: ['', Validators.required]
     });
   }
 
   ngOnInit(): void {
+    this.livroService.findClassificacoes().subscribe(data => {
+      this.classificacoes = data;
+      this.initializeForm();
+    });
     this.generoService.findAll().subscribe(data => {
       this.generos = data;
       this.initializeForm();
-    }),
+    });
     this.fornecedorService.findAll().subscribe(data => {
       this.fornecedores = data;
       this.initializeForm();
-    }),
+    });
     this.editoraService.findAll().subscribe(data => {
       this.editoras = data;
       this.initializeForm();
-    }),
+    });
     this.autorService.findAll().subscribe(data => {
       this.autores = data;
       this.initializeForm();
-    })
+    });
+    this.initializeForm();
+
   }
 
   initializeForm(): void {
     const livro: Livro = this.activatedRoute.snapshot.data['livro'];
-    // console.log("Inicialize form");
-    // console.log(livro);
 
     const fornecedor = this.fornecedores.find(fornecedor => fornecedor.id === (livro?.fornecedor?.id || null));
     const editora = this.editoras.find(editora => editora.id === (livro?.editora?.id || null));
-
-    // console.log("Inicialize form - genero");
-    // console.log(livro.generos);
+    const classificacao = this.classificacoes.find(c => c.id === (livro?.classificacao?.id || null));
+    
+    // carregando a imagem do preview
+    if (livro && livro.nomeImagem) {
+      this.imagePreview = this.livroService.getUrlImage(livro.nomeImagem);
+      this.fileName = livro.nomeImagem;
+    }
 
     this.formGroup = this.formBuilder.group({
       id: [(livro && livro.id) ? livro.id : null],
@@ -114,22 +128,51 @@ export class LivroFormComponent implements OnInit {
       editora: [editora, Validators.required],
       generos: [(livro && livro.generos) ? livro.generos.map((genero) => genero.id) : [], Validators.required],
       autores: [(livro && livro.autores) ? livro.autores.map((autor) => autor.id) : [], Validators.required],
-      classificacao: [(livro && livro.classificacao) ? livro.classificacao : null, Validators.required],
+      classificacao: [classificacao, Validators.required],
       datalancamento: [(livro && livro.datalancamento) ? livro.datalancamento : null, Validators.required]
     });
   }
 
-  tratarErros(errorResponse: HttpErrorResponse){
-    if(errorResponse.status === 400){
-      if(errorResponse.error?.errors){
+  carregarImagemSelecionada(event: any) {
+    this.selectedFile = event.target.files[0];
+
+    if (this.selectedFile) {
+      this.fileName = this.selectedFile.name;
+      // carregando image preview
+      const reader = new FileReader();
+      reader.onload = e => this.imagePreview = reader.result;
+      reader.readAsDataURL(this.selectedFile);
+    }
+  }
+
+  private uploadImage(livroId: number) {
+    if (this.selectedFile) {
+      this.livroService.uploadImage(livroId, this.selectedFile.name, this.selectedFile)
+        .subscribe({
+          next: () => {
+            this.voltarPagina();
+          },
+          error: err => {
+            console.log('Erro ao fazer o upload da imagem');
+            // tratar o erro
+          }
+        })
+    } else {
+      this.voltarPagina();
+    }
+  }
+
+  tratarErros(errorResponse: HttpErrorResponse) {
+    if (errorResponse.status === 400) {
+      if (errorResponse.error?.errors) {
         errorResponse.error.errors.forEach((validationError: any) => {
           const formControl = this.formGroup.get(validationError.fieldName);
-          if(formControl){
-            formControl.setErrors({apiError: validationError.message})
+          if (formControl) {
+            formControl.setErrors({ apiError: validationError.message })
           }
         });
       }
-    } else if (errorResponse.status < 400){
+    } else if (errorResponse.status < 400) {
       alert(errorResponse.error?.message || 'Erro genérico do envio do formulário.');
     } else if (errorResponse.status >= 500) {
       alert('Erro do servidor.');
@@ -141,18 +184,19 @@ export class LivroFormComponent implements OnInit {
     if (this.formGroup.valid) {
       const livro = this.formGroup.value;
       const operacao = livro.id == null
-      ? this.livroService.insert(livro)
-      : this.livroService.update(livro);
+        ? this.livroService.insert(livro)
+        : this.livroService.update(livro);
 
       operacao.subscribe({
-        next: () => {
+        next: (livroCadastrado) => {
+          this.uploadImage(livroCadastrado.id);
           this.router.navigateByUrl('/admin/livros');
-          this.snackBar.open('O Livro foi salvo com Sucesso!!', 'Fechar', {duration: 3000});
+          this.snackBar.open('O Livro foi salvo com Sucesso!!', 'Fechar', { duration: 3000 });
         },
         error: (error) => {
           console.error('Erro ao Salvar' + JSON.stringify(error));
           this.tratarErros(error);
-          this.snackBar.open('Erro ao tentar salvar o Livro', 'Fechar', {duration: 3000});
+          this.snackBar.open('Erro ao tentar salvar o Livro', 'Fechar', { duration: 3000 });
         }
       });
     }
@@ -171,11 +215,11 @@ export class LivroFormComponent implements OnInit {
             this.livroService.delete(livro).subscribe({
               next: () => {
                 this.router.navigateByUrl('/admin/livros');
-                this.snackBar.open('O Livro foi excluído com Sucesso!!', 'Fechar', {duration: 3000});
+                this.snackBar.open('O Livro foi excluído com Sucesso!!', 'Fechar', { duration: 3000 });
               },
               error: (err) => {
                 console.log('Erro ao excluir' + JSON.stringify(err));
-                this.snackBar.open('Erro ao tentar excluir o Livro', 'Fechar', {duration: 3000});
+                this.snackBar.open('Erro ao tentar excluir o Livro', 'Fechar', { duration: 3000 });
               }
             });
           }
@@ -271,5 +315,8 @@ export class LivroFormComponent implements OnInit {
   }
   voltar() {
     this.router.navigateByUrl('/admin/livros');
+  }
+  voltarPagina() {
+    this.location.back();
   }
 }
