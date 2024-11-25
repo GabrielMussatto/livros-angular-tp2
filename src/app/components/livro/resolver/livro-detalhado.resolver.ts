5752db63e9f145a50ee24951438dce34f7b38939
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-11-25 14:24:06 -0300

    ajuste no resolver de livroDetalhado

--- src/app/components/livro/resolver/livro-detalhado.resolver.ts
+++ src/app/components/livro/resolver/livro-detalhado.resolver.ts
@@ -5,6 +5,6 @@ import { Livro } from '../../../models/livro.model';
 import { LivroService } from '../../../services/livro.service';
 
 export const livroDetalhadoResolver: ResolveFn<Livro> = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-  return inject(LivroService).findByNome(route.paramMap.get('titulo')!)
+  return inject(LivroService).findByTitulo(route.paramMap.get('titulo')!)
     .pipe(map(livro => livro[0]));
 };
