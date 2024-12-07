276a40f750dc09fa1b358468ec95fd2cd79f2c3a
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-12-07 01:53:16 -0300

    criando interfaces de pedido e item pedido

---
+++ src/app/models/item-pedido.ts
@@ -0,0 +1,9 @@
+export interface ItemPedido {
+    idLivro?: number; // Caso seja um livro
+    idCaixaLivro?: number;   // Caso seja uma caixa de livro
+    titulo?: string;
+    preco?: number;
+    quantidade: number;
+    desconto?: number;
+    subTotal?: number;
+}
