a847f4b7bc65fa8207e183350370f8412db93bcb
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-12-12 18:45:30 -0300

    Atualizando models de cliente e usuario

--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -1,6 +1,15 @@
+import { Sexo } from "./sexo.model";
+import { Telefone } from "./telefone.model";
+
 export class Usuario{
     id!: number;
     nome!: string;
     username!: string;
     perfil!: number;
+    senha!: string;
+    dataNascimento!: Date;
+    email!: string;
+    cpf!: string;
+    telefone!: Telefone[];
+    idSexo!: Sexo;
 }
