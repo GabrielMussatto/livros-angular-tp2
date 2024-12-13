6c3e89d316eccd3781cd93dde794c5f6331382f7
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-12-13 03:01:40 -0300

    Ajuste em model e service de funcionario

--- src/app/models/funcionario.model.ts
+++ src/app/models/funcionario.model.ts
@@ -1,6 +1,7 @@
 import { Usuario } from "./usuario.model";
 
 export class Funcionario{
+    id!: number;
     usuario!: Usuario;
     salario!: number;
     cargo!: string;
--- src/app/services/funcionario.service.ts
+++ src/app/services/funcionario.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Sugestao } from '../models/sugestao.model';
+import { Funcionario } from '../models/funcionario.model';
 
 @Injectable({
   providedIn: 'root'
@@ -24,12 +25,83 @@ export class FuncionarioService {
     return new HttpHeaders().set('Authorization', `Bearer ${token}`);
   }
 
-  // Buscar Minhas Sugestões
   listarSugestoes(): Observable<Sugestao[]> {
     const headers = this.getHeaders();
     return this.httpClient.get<Sugestao[]>(
       `${this.baseUrl}/sugestoes/listarSugestoes`,
       { headers }
     );
+  }
+
+  findAll(page: number, pageSize: number): Observable<Funcionario[]> {
+    const headers = this.getHeaders();
+    const params = {
+      page: page.toString(),
+      pageSize: pageSize.toString(),
+    };
+    return this.httpClient.get<Funcionario[]>(this.baseUrl, { headers, params });
+  }
+
+  findById(id: string): Observable<Funcionario> {
+    return this.httpClient.get<Funcionario>(`${this.baseUrl}/${id}`);
+  }
+
+  findByNome(nome: string, page: number, pageSize: number): Observable<Funcionario[]> {
+    const headers = this.getHeaders();
+    const params = {
+      nome,
+      page: page.toString(),
+      pageSize: pageSize.toString(),
+    };
+    return this.httpClient.get<Funcionario[]>(`${this.baseUrl}/search/nome`, { headers, params });
+  }
+
+  countByNome(nome: string): Observable<number> {
+    const headers = this.getHeaders();
+    const params = { nome };
+    return this.httpClient.get<number>(`${this.baseUrl}/count/nome`, { headers, params });
+  }
+
+  // Método para contar o total de clientes
+  count(): Observable<number> {
+    const headers = this.getHeaders();
+    return this.httpClient.get<number>(`${this.baseUrl}/count`, { headers });
+  }
+
+  insert(funcionario: Funcionario): Observable<Funcionario> {
+    const data = {
+      salario: funcionario.salario,
+      cargo: funcionario.cargo,
+      nome: funcionario.usuario.nome,
+      username: funcionario.usuario.username,
+      senha: funcionario.usuario.senha,
+      dataNascimento: funcionario.usuario.dataNascimento,
+      email: funcionario.usuario.email,
+      cpf: funcionario.usuario.cpf,
+      telefone: funcionario.usuario.telefone,
+      idSexo: funcionario.usuario.idSexo.id
+    }
+    return this.httpClient.post<Funcionario>(this.baseUrl, data);
+  }
+
+  // Atualizar um cliente
+  update(funcionario: Funcionario): Observable<void> {
+    const data = {
+      salario: funcionario.salario,
+      cargo: funcionario.cargo,
+      nome: funcionario.usuario.nome,
+      username: funcionario.usuario.username,
+      senha: funcionario.usuario.senha,
+      dataNascimento: funcionario.usuario.dataNascimento,
+      email: funcionario.usuario.email,
+      cpf: funcionario.usuario.cpf,
+      telefone: funcionario.usuario.telefone,
+      idSexo: funcionario.usuario.idSexo.id
+    }
+    return this.httpClient.put<void>(`${this.baseUrl}/${funcionario.id}`, data);
+  }
+
+  delete(funcionario: Funcionario): Observable<any> {
+    return this.httpClient.delete<any>(`${this.baseUrl}/${funcionario.id}`);
   }
 }
