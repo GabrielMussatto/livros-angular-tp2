6ec044e3f0d06efc670260926df365f9dfcebedd
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-12-14 03:53:38 -0300

    Criando model de cartao de credito

---
+++ src/app/models/cartao-credito.model.ts
@@ -0,0 +1,8 @@
+export class CartaoCredito {
+    numeroCartao!: string;
+    nomeImpressaoTitular!: string;
+    cvc!: number;
+    validade!: string;
+    cpfTitular!: string;
+    bandeiraCartao!: number;
+}
